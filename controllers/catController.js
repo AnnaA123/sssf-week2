ad4bea2a4009a4cba7511e5b8d2ad6def2a1bc36
Anna <anna.alava2@metropolia.fi>
2021-03-17 16:00:29 +0200

    update

--- controllers/catController.js
+++ controllers/catController.js
@@ -9,13 +9,8 @@ const cat_list_get = (req, res) => {
 };
 
 const cat_get = (req, res) => {
-  res.json(cats.filter(({id}) => res.params.id));
-  //res.json(cats.filter(check_cat));
-  //res.send(cats.filter(check_cat));
-  /*res.send('You asked for cat number ' + req.params.id + '. JSON ');
-  cat_list = res.json(cats);
-
-  cat = cat_list.filter(check_cat);*/
+  let cat = JSON.parse(JSON.stringify(cats));
+  res.send(cat.filter((kitty) => kitty.id === req.params.id));
 };
 
 module.exports = {
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,6 +8,12 @@ const user_list_get = (req, res) => {
   res.json(users);
 };
 
+const user_get = (req, res) => {
+  let user = JSON.parse(JSON.stringify(users));
+  res.send(user.filter((use) => use.id === req.params.id));
+};
+
 module.exports = {
   user_list_get,
+  user_get,
 };
